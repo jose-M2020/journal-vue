e3e5942addacb4fd43639aaf47aab17c3e9bd99c
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-10-12 21:57:53 -0600

    [add] helpers, [update] store, views

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView'
+import Home from '../views/Home.vue'
 
 import daybookRouter from '../modules/daybook/router'
 
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
@@ -15,7 +15,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/daybook',
